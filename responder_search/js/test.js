76b7c900f2bf8b87c2b9cec547c59902c51d4136
lifedinosaur <seanbrutscher@gmail.com>
2017-06-23 09:26:15 -0400

    Remove front-end filtering todo test.

--- responder_search/js/test.js
+++ responder_search/js/test.js
@@ -17,9 +17,3 @@
 QUnit.test( 'App JavaScript loaded.', function( assert ) {
   assert.ok(typeof App.displayResults === 'function', 'Passed!' );
 });
-
-QUnit.test( 'Filtering test.', function( assert ) {
-  // @TODO A a test or two to verify your filtering functionality works.
-  // See http://api.qunitjs.com for more details on how to use QUnit.
-  assert.ok(false, 'Passed!' );
-});
